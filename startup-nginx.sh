b200041ec1ede50a07ad9a9ac818cc88355b47ab
Daniel Fox <romaimperator@gmail.com>
2017-03-18 21:11:02 -0500

    initial copy from the other guy

---
+++ startup-nginx.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+
+set -x
+
+chown -R www-data:www-data /var/www/rutorrent
+cp /downloads/.htpasswd /var/www/rutorrent/
+mkdir -p /downloads/.rutorrent/torrents
+chown -R www-data:www-data /downloads/.rutorrent
+
+rm -f /etc/nginx/sites-enabled/*
+
+rm -rf /etc/nginx/ssl
+
+rm /var/www/rutorrent/.htpasswd
+
+
+# Basic auth enabled by default
+site=rutorrent-basic.nginx
+
+# Check if TLS needed
+if [[ -e /downloads/nginx.key && -e /downloads/nginx.crt ]]; then
+mkdir -p /etc/nginx/ssl
+cp /downloads/nginx.crt /etc/nginx/ssl/
+cp /downloads/nginx.key /etc/nginx/ssl/
+site=rutorrent-tls.nginx
+fi
+
+cp /root/$site /etc/nginx/sites-enabled/
+[ -n "$NOIPV6" ] && sed -i 's/listen \[::\]:/#/g' /etc/nginx/sites-enabled/$site
+
+# Check if .htpasswd presents
+if [ -e /downloads/.htpasswd ]; then
+cp /downloads/.htpasswd /var/www/rutorrent/ && chmod 755 /var/www/rutorrent/.htpasswd && chown www-data:www-data /var/www/rutorrent/.htpasswd
+else
+# disable basic auth
+sed -i 's/auth_basic/#auth_basic/g' /etc/nginx/sites-enabled/$site
+fi
+
+nginx -g "daemon off;"
---
+++ startup-php.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -x
+
+MEM=${PHP_MEM:=256M}
+
+sed -i 's/memory_limit.*$/memory_limit = '$MEM'/g' /etc/php/7.0/fpm/php.ini
+
+mkdir /run/php
+php-fpm7.0 --nodaemonize
+
